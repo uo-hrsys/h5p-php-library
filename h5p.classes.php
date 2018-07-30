ac12801bf5ec58c36c4ca60557d12ff5e8233019
thomasmars <thomasmars87@gmail.com>
2018-07-30 14:37:05 +0200

    JI-781 Bump core version, since getCrossOrigin() signature changed

--- h5p.classes.php
+++ h5p.classes.php
@@ -1803,7 +1803,7 @@ class H5PCore {
   
   public static $coreApi = array(
     'majorVersion' => 1,
-    'minorVersion' => 17
+    'minorVersion' => 18
   );
   public static $styles = array(
     'styles/h5p.css',
